24c1d7fce1b0f9d9ae517af8ae81a11947aa09b5
Klevis <klevis.maze@gmail.com>
2025-01-26 01:59:59 +0100

    .

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -3,10 +3,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Importing images for tours
-import image1 from "../images/image1.jpg";
-import image2 from "../images/image2.jpg";
-import image3 from "../images/image3.jpg";
-import image4 from "../images/image4.jpg";
 import image5 from "../images/image5.jpg";
 
 // Tours list
